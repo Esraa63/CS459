78b309eb64e05b44804b6938d3b16830c9dfe5b8
Esraa63 <61542189+Esraa63@users.noreply.github.com>
2020-04-16 18:25:45 +0200

    no
    Co-Authored-By: Yousef Allam <yousef.allam@hotmail.com>

--- Web/NewStudent.aspx.cs
+++ Web/NewStudent.aspx.cs
@@ -12,19 +12,19 @@ protected void Button1_Click(object sender, EventArgs e)
 {
     SqlConnection conn = new SqlConnection();
     conn.ConnectionString = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|WbeDB.mdf;Integrated Security=True";
-    string strInsert = "INSERT INTO [dbo].[Student] " + " VALUES('" 
+    string strInsert = "INSERT INTO [dbo].[Student]" + "VALUES('" + iD.Text + "', '"
             + fN.Text + "', '"
             + lN.Text + "', '"
-            + iD.Text + "', '"
             + RadioButtonList1.SelectedValue + "', '"
             + Calendar1.SelectedDate + "', '"
             + em.Text + "', '"
             + pho.Text + "', '"
-            + add.Text + "', '"
+            + TextArea1.Text + "', '"
             + pass.Text + "', '"
             + na.Text + "', '"
             + DropDownList1.SelectedValue + "', '"
             + CheckBox1.Text + "')";
+
     SqlCommand cmdInsert = new SqlCommand(strInsert, conn);
     try
     {
